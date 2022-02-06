021468daf7a805231d480d58f71e76786684245c
Mike Iversen <miversen33@gmail.com>
2022-02-06 13:24:10 -0600

    Added Debug Mode to config and hooks for split out keymaps

--- editors/nvim/init.lua
+++ editors/nvim/init.lua
@@ -7,19 +7,23 @@
 -- wget https://github.com/rust-analyzer/rust-analyzer/releases/download/2021-12-27/rust-analyzer-x86_64-unknown-linux-gnu.gz (set a location for this to be installed to)
 -- wget https://github.com/sumneko/lua-language-server/releases/download/2.5.6/lua-language-server-2.5.6-linux-x64.tar.gz (set a location for this to be installed to)
 -- TODO(Mike) (Setup java language server?)
+-- BUG(Mike) There is something causing line tearing in here :/
 
--- Packer Bootstrapping
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+local DEBUG = false
+
+if not DEBUG then
+    -- Packer Bootstrapping
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+      packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    end
+
+    -- Custom Functions
+
+    -- Plugin setup
+    require('plugins')
 end
-
--- Custom Functions
-
--- Plugin setup
-require('plugins')
-
 -- Neovim Configurations
 -- Create a generic "enable italics, enable bold, enable transparent" map so we can auto do that
 -- for any themes we set
@@ -58,96 +62,96 @@ vim.opt.smarttab      = true
 vim.o.completeopt     = 'longest,preview,menuone,noselect'
 
 -- Keymaps
-require('keymaps')
+require('keymaps').setup(DEBUG)
 
--- Language Server Setup
-local illuminate = require('illuminate')
+if not DEBUG then
+    -- Language Server Setup
+    local illuminate = require('illuminate')
 
-local M = {}
+    local M = {}
 
-M.icons = {
-  Class = " ",
-  Color = " ",
-  Constant = " ",
-  Constructor = " ",
-  Enum = "了 ",
-  EnumMember = " ",
-  Field = " ",
-  File = " ",
-  Folder = " ",
-  Function = " ",
-  Interface = "ﰮ ",
-  Keyword = " ",
-  Method = "ƒ ",
-  Module = " ",
-  Property = " ",
-  Snippet = "﬌ ",
-  Struct = " ",
-  Text = " ",
-  Unit = " ",
-  Value = " ",
-  Variable = " ",
-}
+    M.icons = {
+      Class = " ",
+      Color = " ",
+      Constant = " ",
+      Constructor = " ",
+      Enum = "了 ",
+      EnumMember = " ",
+      Field = " ",
+      File = " ",
+      Folder = " ",
+      Function = " ",
+      Interface = "ﰮ ",
+      Keyword = " ",
+      Method = "ƒ ",
+      Module = " ",
+      Property = " ",
+      Snippet = "﬌ ",
+      Struct = " ",
+      Text = " ",
+      Unit = " ",
+      Value = " ",
+      Variable = " ",
+    }
 
-function M.setup()
-  local kinds = vim.lsp.protocol.CompletionItemKind
-  for i, kind in ipairs(kinds) do
-    kinds[i] = M.icons[kind] or kind
-  end
-end
-
-vim.cmd [[autocmd ColorScheme * highlight NormalFloat guibg=#1f2335]]
-vim.cmd [[autocmd ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
-
-local handlers = {
-  ['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {border = 'rounded'}),
-  ["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {border = 'rounded' }),
-}
-
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-local lsp_on_attach = function(client)
-  illuminate.on_attach(client)
-end
-
--- TODO(Mike): Consider maybe using null-ls?
-
-
-local ls_settings_available, _ = pcall(require, 'ls_settings')
-
-if ls_settings_available then
-    local lspconfig = require('lspconfig')
-    local language_servers = require('ls_settings').get_lsp_settings()
-    for language_server, language_server_config in pairs(language_servers) do
-        local on_attach = language_server_config['on_attach']
-        if(on_attach) then
-            language_server_config['on_attach'] = function(client)
-                lsp_on_attach(client)
-                on_attach(client)
-            end
-        else
-            language_server_config['on_attach'] = lsp_on_attach
-        end
-        language_server_config['capabilities'] = capabilities
-        language_server_config['handlers'] = handlers
-        lspconfig[language_server].setup(language_server_config)
+    function M.setup()
+      local kinds = vim.lsp.protocol.CompletionItemKind
+      for i, kind in ipairs(kinds) do
+        kinds[i] = M.icons[kind] or kind
+      end
     end
+
+    vim.cmd [[autocmd ColorScheme * highlight NormalFloat guibg=#1f2335]]
+    vim.cmd [[autocmd ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
+
+    local handlers = {
+      ['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {border = 'rounded'}),
+      ["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {border = 'rounded' }),
+    }
+
+    local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    local lsp_on_attach = function(client)
+      illuminate.on_attach(client)
+    end
+
+    -- TODO(Mike): Consider maybe using null-ls?
+
+
+    local ls_settings_available, _ = pcall(require, 'ls_settings')
+
+    if ls_settings_available then
+        local lspconfig = require('lspconfig')
+        local language_servers = require('ls_settings').get_lsp_settings()
+        for language_server, language_server_config in pairs(language_servers) do
+            local on_attach = language_server_config['on_attach']
+            if(on_attach) then
+                language_server_config['on_attach'] = function(client)
+                    lsp_on_attach(client)
+                    on_attach(client)
+                end
+            else
+                language_server_config['on_attach'] = lsp_on_attach
+            end
+            language_server_config['capabilities'] = capabilities
+            language_server_config['handlers'] = handlers
+            lspconfig[language_server].setup(language_server_config)
+        end
+    end
+
+    local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    end
+
+    vim.diagnostic.config({
+      update_in_insert = true,
+      virtual_text = {
+        source = "always",
+        prefix = '●'
+      },
+      float = {
+        source = "always",
+      },
+    })
 end
-
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
-
-vim.diagnostic.config({
-  update_in_insert = true,
-  virtual_text = {
-    source = "always",
-    prefix = '●'
-  },
-  float = {
-    source = "always",
-  },
-})
-
--- We will want to look into how to load "dap" settings on a per project/directory/file basis. Right now we have _nothing_
--- editors/nvim/lua/keymaps.lua
+++ editors/nvim/lua/keymaps.lua
@@ -1,60 +1,10 @@
-local silent_noremap = {noremap = true, silent = true}
-local noremap = {noremap=true}
-
-local function map(kind, lhs, rhs, opts)
-    vim.api.nvim_set_keymap(kind, lhs, rhs, opts)
+local setup = function(debug_mode)
+    require('core_keymaps')
+    if not debug_mode then
+        require('plug_keymaps')
+    end
 end
 
-local telescope = require('telescope')
-
--- Keymappings
-map('n', '<Space',   '<Nop>', silent_noremap)
-vim.g.mapleader = ' '
-
-map('n', '<A-k>',    ':m .+1<CR>==', silent_noremap)
-map('n', '<A-i>',    ':m .-2<CR>==', silent_noremap)
-map('n', '<C-u>',    ':tabp<CR>:echo "Tab Left"<CR>' , noremap)
-map('n', '<C-o>',    ':tabn<CR>:echo "Tab Right"<CR>', noremap)
-map('n', '<A-j>',    ':bprevious<CR>:echo "Buffer Left"<CR>', noremap)
-map('n', '<A-l>',    ':bnext<CR>:echo "Buffer Right"<CR>', noremap)
-map('n', '<C-x>',    'dd', silent_noremap)
-map('n', '<C-X>',    'ddO', silent_noremap)
-map('n', '<C-j>',    '<C-W><C-h>:echo "Pane Left"<CR>', noremap)
-map('n', '<C-l>',    '<C-W><C-l>:echo "Pane Right"<CR>', noremap)
-map('n', '<C-i>',    '<C-W><C-k>:echo "Pane Up"<CR>', noremap)
-map('n', '<C-k>',    '<C-W><C-j>:echo "Pane Down"<CR>', noremap)
-map('n', '<C-s>',    ':w<CR> :echo "Saved File"<CR>', noremap)
-map('i', '<C-s>',    '<esc>:w<CR> :echo "Saved File"<CR>', noremap)
-map('i', '<S-Tab>',  '<esc>:<<<CR>i', silent_noremap)
-map('n', '<S-Tab>',  ':<<<CR>', silent_noremap)
-map('n', '<C-f>',    '/', noremap)
-map('i', '<C-f>',    '<esc>/', noremap)
-map('n', '<leader>f',':noh<CR>', silent_noremap)
-map('n', '<S-f>',    ':Telescope live_grep<CR>', noremap)
-map('n', '<C-z>',    ':Telescope find_files<CR>', silent_noremap)
-map('n', '<S-z>',    ':Telescope live_grep<CR>', silent_noremap)
-map('n', '<C-T>',    ':TroubleToggle<CR>', silent_noremap)
-map('n', '<C-h>',    ':lua vim.lsp.buf.hover()<CR>', silent_noremap)
-map('n', '<A-left>', ':vertical resize -5<CR>', silent_noremap)
-map('n', '<A-right>',':vertical resize +5<CR>', silent_noremap)
-map('n', '<A-down>', ':resize -5<CR>', silent_noremap)
-map('n', '<A-up>',   ':resize +5<CR>', silent_noremap)
-map('n', '<C-_>',    ':lua require("Comment.api").toggle_current_linewise()<CR>', silent_noremap)
-map('i', '<C-_>',    '<ESC>:lua require("Comment.api").toggle_current_linewise()<CR>i', silent_noremap)
-map('x', '<C-_>',    '<ESC>:lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<CR>', silent_noremap)
-map('n', '<C-n>',    ':lua require("illuminate").next_reference{wrap=true}<CR>', silent_noremap)
-map('n', '<S-n>',    ':lua require("illuminate").next_reference{reverse=true,wrap=true}<CR>', silent_noremap)
-map('n', '<A-fr>',   ':echo "Formatting Buffer"<CR> :lua vim.lsp.buf.formatting()<CR>', silent_noremap)
-map('n', '<A-f>',    ':Telescope find_files<CR>', silent_noremap)
--- map('n', '<Enter>',  ':SymbolsOutline<CR>', silent_noremap)
-map('n', '<Enter>',  ':Vista!!<CR>', silent_noremap)
-map('n', '<leader>n',':Telescope file_browser<CR>', silent_noremap)
-map('n', '<leader>r',':lua require("spectre").open_file_search()<CR>', silent_noremap)
-map('n', '<leader>b',':lua require("dap").toggle_breakpoint()<CR>', silent_noremap)
-map('n', '<leader>B',':lua require("dap").set_breakpoint(vim.fn.input("Condition: "))<CR>', silent_noremap)
-map('n', '<F5>',     ':lua require("dap").continue()<CR>', silent_noremap)
-map('n', '<F6>',     ':lua require("dap").run_last()<CR>', silent_noremap)
-map('n', '<leader>up',':lua require("dap").step_out()<CR>', silent_noremap)
-map('n', '<leader>down', ':lua require("dap").step_into()<CR>', silent_noremap)
-map('n', '<leader>right', ':lua require("dap").step_over()<CR>', silent_noremap)
-map('n', '<leader>ro', ':lua require("dap").repl.open()<CR>', silent_noremap)
+return {
+    setup = setup
+}
