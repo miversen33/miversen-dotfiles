f1f337ae9ba0633b451876e8441d5afe7855dd92
Mike Iversen <miversen33@gmail.com>
2022-06-06 06:19:31 -0500

    Fixed icky code

--- editors/nvim/init.lua
+++ editors/nvim/init.lua
@@ -15,7 +15,7 @@ if not DEBUG then
     local fn = vim.fn
     local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
     if fn.empty(fn.glob(install_path)) > 0 then
-      packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+      fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
     end
 
     -- Custom Functions
