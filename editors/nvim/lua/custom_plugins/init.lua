7bd2096481bc0c5ae4e8e4f200a69607dc3d53d7
Mike Iversen <miversen33@gmail.com>
2022-08-07 12:04:36 -0500

    Forgot to add custom_plugins init loader

---
+++ editors/nvim/lua/custom_plugins/init.lua
@@ -0,0 +1,18 @@
+require("import")
+local custom_plugins_path = vim.fn.stdpath('config') .. '/lua/custom_plugins/'
+local dir_handle = vim.loop.fs_scandir(custom_plugins_path)
+-- Nothing like a good ole little bit of import path clobbering
+-- ay Mr. Squidward?
+package.path = custom_plugins_path .. "?.lua;" .. package.path
+while true do
+    local item, _ = vim.loop.fs_scandir_next(dir_handle)
+    if not item then break end
+    item = item:match('(.+)%..+')
+    if not item then goto continue end
+    if item:match('^%s*$') or item:match('init') then
+        goto continue
+    end
+    print("Importing ", item)
+    import(item)
+    ::continue::
+end
