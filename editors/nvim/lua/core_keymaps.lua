5b56b713c30803e32f8df6a9a8db29b77686fc53
Mike Iversen <miversen33@gmail.com>
2022-10-01 22:10:07 -0500

    Converted to vim.keymap.set

--- editors/nvim/lua/core_keymaps.lua
+++ editors/nvim/lua/core_keymaps.lua
@@ -1,31 +1,24 @@
-local silent_noremap = {noremap = true, silent = true}
-local noremap = {noremap=true}
-
-local function map(kind, lhs, rhs, opts)
-    vim.api.nvim_set_keymap(kind, lhs, rhs, opts)
-end
-
 -- Keymappings
-map('n', '<Space>',   '<Nop>', silent_noremap)
+vim.keymap.set('n', '<Space>',   '<Nop>', {silent = true})
 vim.g.mapleader = ' '
 
 -- Really not fond of this
-map('', '<Esc>',     ':noh<CR><Esc>', silent_noremap)
-map('n', 'AA',       'ggVG', silent_noremap)
-map('v', 'AA',       'ggV', silent_noremap)
-map('n', '<A-Down>', 'ddjP', silent_noremap)
-map('n', '<A-Up>',   'ddkP', silent_noremap)
-map('n', '<C-x>',    'dd', silent_noremap)
-map('n', '<C-X>',    'ddO', silent_noremap)
-map('n', '<C-s>',    ':w<CR> :echo "Saved File"<CR>h', noremap)
-map('i', '<C-s>',    '<esc>:w<CR> :echo "Saved File"<CR>', noremap)
-map('i', '<S-Tab>',  '<esc>:<<CR>i', silent_noremap)
-map('n', '<S-Tab>',  ':<<CR>', silent_noremap)
-map('n', '<Up>',     'gk', silent_noremap)
-map('n', '<Down>',   'gj', silent_noremap)
-map('v', '<Up>',     'gk', silent_noremap)
-map('v', '<Down>',   'gj', silent_noremap)
-map('v', '<Tab>',    ':><CR>', silent_noremap)
-map('v', '<S-Tab>',  ':<<CR>', silent_noremap)
-map('n', '<C-Del>',  'dw', silent_noremap)
-map('i', '<C-Del>',  '<esc>ldwi', silent_noremap)
+vim.keymap.set('', '<Esc>',     ':noh<CR><Esc>', {silent = true})
+vim.keymap.set('n', 'AA',       'ggVG', {silent = true})
+vim.keymap.set('v', 'AA',       'ggV', {silent = true})
+vim.keymap.set('n', '<A-Down>', 'ddjP', {silent = true})
+vim.keymap.set('n', '<A-Up>',   'ddkP', {silent = true})
+vim.keymap.set('n', '<C-x>',    'dd', {silent = true})
+vim.keymap.set('n', '<C-X>',    'ddO', {silent = true})
+vim.keymap.set('n', '<C-s>',    ':w<CR> :echo "Saved File"<CR>h')
+vim.keymap.set('i', '<C-s>',    '<esc>:w<CR> :echo "Saved File"<CR>')
+vim.keymap.set('i', '<S-Tab>',  '<esc>:<<CR>i', {silent = true})
+vim.keymap.set('n', '<S-Tab>',  ':<<CR>', {silent = true})
+vim.keymap.set('n', '<Up>',     'gk', {silent = true})
+vim.keymap.set('n', '<Down>',   'gj', {silent = true})
+vim.keymap.set('v', '<Up>',     'gk', {silent = true})
+vim.keymap.set('v', '<Down>',   'gj', {silent = true})
+vim.keymap.set('v', '<Tab>',    ':><CR>', {silent = true})
+vim.keymap.set('v', '<S-Tab>',  ':<<CR>', {silent = true})
+vim.keymap.set('n', '<C-Del>',  'dw', {silent = true})
+vim.keymap.set('i', '<C-Del>',  '<esc>ldwi', {silent = true})
