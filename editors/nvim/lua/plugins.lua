2700a711ed946049b7af7433b4c7dd7497117a21
Mike Iversen <miversen33@gmail.com>
2022-10-03 12:14:51 -0500

    Setup notify to takeover print and notify

--- editors/nvim/lua/plugins.lua
+++ editors/nvim/lua/plugins.lua
@@ -178,7 +178,19 @@ require('packer').startup(function(use)
 end)
 
 require("import").config({output_split_type='vertical', import_enable_better_printing=true})
-
+import('notify', function(notify)
+    -- Overriding vim.notify with fancy notify if fancy notify exists
+    vim.notify = notify
+    print = function(...)
+        local print_safe_args = {}
+        local _ = {...}
+        for i=1, #_ do
+            table.insert(print_safe_args, tostring(_[i]))
+        end
+        notify(table.concat(print_safe_args, ' '), "info") end
+    notify.setup({
+    })
+end)
 -- Plugin Configurations
 local excluded_filetypes_array = {'lsp-installer', 'lspinfo', 'Outline', 'help', 'packer', 'netrw', 'qf', 'dbui', 'Trouble', 'fugitive', 'floaterm', 'spectre_panel', 'spectre_panel_write', 'checkhealth', 'man'}
 local excluded_filetypes_table = {}
