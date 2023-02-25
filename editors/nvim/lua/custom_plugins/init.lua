6120319f6fbd0fc48c0e0450b230dda1253ae3a0
Mike Iversen <miversen33@gmail.com>
2023-02-25 11:27:30 -0600

    Fixed file name clobbering

--- editors/nvim/lua/custom_plugins/init.lua
+++ editors/nvim/lua/custom_plugins/init.lua
@@ -1,14 +1,25 @@
 local custom_plugins_path = vim.fn.stdpath('config') .. '/lua/custom_plugins/'
 local dir_handle = vim.loop.fs_scandir(custom_plugins_path)
+
+-- Ensure the custom config exists
+local sysname = vim.loop.os_uname().sysname:lower()
+local sep = sysname:match('windows') and '\\' or '/' -- \ for windows, mac and linux both use \
+local config = string.format("%s%s%s", vim.fn.stdpath('data'), sep, 'custom_config')
+vim.fn.mkdir(config, 'p', '0o700')
+local _ = io.open(string.format("%s%s%s", config, sep, "config.json"), "w+")
+assert(_, "Unable to open custom config")
+assert(_:close(), "Unable to close custom config")
+
 -- Nothing like a good ole little bit of import path clobbering
 -- ay Mr. Squidward?
 package.path = custom_plugins_path .. "?.lua;" .. package.path
 while true do
     local item, _ = vim.loop.fs_scandir_next(dir_handle)
     if not item then break end
-    item = item:match('(.+)%..+')
+    _ = item
+    item = item:match('(.+)%.lua$')
     if not item then goto continue end
-    if item:match('^%s*$') or item:match('init') then
+    if item:match('init') then
         goto continue
     end
     import(item)
