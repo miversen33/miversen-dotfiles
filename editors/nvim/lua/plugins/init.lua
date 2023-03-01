0e0334cdbf4e47ff2e584a675ed15c59f9bca09a
Mike Iversen <miversen33@gmail.com>
2023-02-28 19:27:55 -0600

    Added hslense

--- editors/nvim/lua/plugins/init.lua
+++ editors/nvim/lua/plugins/init.lua
@@ -62,6 +62,41 @@ local function get_plugins()
             "folke/trouble.nvim",
             config = true
         },
+        {
+            "kevinhwang91/nvim-hlslens",
+            config = function()
+                local hlslens = require("hlslens")
+                hlslens.setup({
+                    override_lens = function(render, posList, nearest, idx, relIdx)
+                        local sfw = vim.v.searchforward == 1
+                        local indicator, text, chunks
+                        local absRelIdx = math.abs(relIdx)
+                        if absRelIdx > 1 then
+                            indicator = ('%d%s'):format(absRelIdx, sfw ~= (relIdx > 1) and '▲' or '▼')
+                        elseif absRelIdx == 1 then
+                            indicator = sfw ~= (relIdx == 1) and '▲' or '▼'
+                        else
+                            indicator = ''
+                        end
+
+                        local lnum, col = unpack(posList[idx])
+                        if nearest then
+                            local cnt = #posList
+                            if indicator ~= '' then
+                                text = ('[%s %d/%d]'):format(indicator, idx, cnt)
+                            else
+                                text = ('[%d/%d]'):format(idx, cnt)
+                            end
+                            chunks = { { ' ', 'Ignore' }, { text, 'HlSearchLensNear' } }
+                        else
+                            text = ('[%s %d]'):format(indicator, idx)
+                            chunks = { { ' ', 'Ignore' }, { text, 'HlSearchLens' } }
+                        end
+                        render.setVirt(0, lnum - 1, col - 1, chunks, nearest)
+                    end
+                })
+            end
+        },
         -- Themes
         {
             "Mofiqul/vscode.nvim", -- Vscode type theme
