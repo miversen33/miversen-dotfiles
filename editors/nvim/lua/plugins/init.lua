79a35a13ce0ac210a2c6771ca05cf7eb4f094c34
Mike Iversen <miversen33@gmail.com>
2023-04-13 09:14:22 -0500

    Added treesitter playground

--- editors/nvim/lua/plugins/init.lua
+++ editors/nvim/lua/plugins/init.lua
@@ -564,12 +564,38 @@ local function get_plugins()
         },
         {
             "nvim-treesitter/nvim-treesitter", -- Neovim treesitter
-            build = ":TSUpdate",
+            dependencies = {
+                'nvim-treesitter/playground'
+            },
+            build = ":TSUpdate|TSInstall query",
             config = function()
                 require("nvim-treesitter.configs").setup({
                     -- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
                     highlight = { enable = true },
                     markid = { enable = true },
+                    playground = {
+                        enable = true,
+                        disable = {},
+                        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+                        persist_queries = false, -- Whether the query persists across vim sessions
+                        keybindings = {
+                            toggle_query_editor = 'o',
+                            toggle_hl_groups = 'i',
+                            toggle_injected_languages = 't',
+                            toggle_anonymous_nodes = 'a',
+                            toggle_language_display = 'I',
+                            focus_language = 'f',
+                            unfocus_language = 'F',
+                            update = 'R',
+                            goto_node = '<cr>',
+                            show_help = '?',
+                        },
+                    },
+                    query_linter = {
+                        enable = true,
+                        use_virtual_text = true,
+                        lint_events = {"BufWrite", "CursorHold"},
+                    },
                 })
             end,
         },
