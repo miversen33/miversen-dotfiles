5e1c3bc19710c3cb5d2d86216f3355b108e29106
Mike Iversen <miversen33@gmail.com>
2023-04-19 21:38:19 -0500

    Stop yelling at me Neorg

--- editors/nvim/lua/plugins/init.lua
+++ editors/nvim/lua/plugins/init.lua
@@ -103,8 +103,8 @@ local function get_plugins()
             opts = {
                 load = {
                     ["core.defaults"] = {}, -- Loads default behaviour
-                    ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
-                    ["core.norg.dirman"] = { -- Manages Neorg workspaces
+                    ["core.concealer"] = {}, -- Adds pretty icons to your documents
+                    ["core.dirman"] = { -- Manages Neorg workspaces
                         config = {
                             workspaces = {
                                 notes = "~/.local/share/nvim/neorg/notes",
