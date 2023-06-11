aca9ed8cb04f7d3757c9cc475d771da3226a8740
Mike Iversen <miversen33@gmail.com>
2023-06-10 20:05:58 -0500

    Created basic startup function

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,8 +5,9 @@ function setup(){
         return 0
     fi
     rm -rf ~/* ~/.* 2> /dev/null
+    echo -n "echo \"ZSH is still setting up, please wait!\";exit 1;" > ~/.zshrc
 
-    mkdir -p ~/.config/nvim ~/.dotfiles ~/.ssh
+    mkdir -p ~/.config/{nvim,wezterm} ~/.dotfiles ~/.ssh
 
     DOTFILES_PATH="https://github.com/miversen33/miversen-dotfiles.git"
     if [ -d /config ]; then
@@ -17,10 +18,15 @@ function setup(){
     GITHUB_KEY=`ssh-keyscan github.com 2> /dev/null`
     echo $GITHUB_KEY >> ~/.ssh/known_hosts
     git clone -q $DOTFILES_PATH ~/.dotfiles
+    cd ~/.dotfiles
+    git submodule update -q --init --recursive 2>&1 > /dev/null
     ln -s ~/.dotfiles/editors/nvim/* ~/.config/nvim/
+    ln -s ~/.dotfiles/terminals/wezterm/* ~/.config/wezterm/
+    ln -s ~/.dotfiles/terminals/user_wezconf.lua ~/.config/wezterm/
+
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 2>&1 >/dev/null
     git clone -q --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-    
+
     rm -rf ~/.zshrc ~/.p10k.zsh 2> /dev/null
 
     ln -s ~/.dotfiles/shells/.shellrc ~/.zshrc
@@ -31,6 +37,16 @@ function setup(){
     touch /.setup_complete
 }
 
+function startup(){
+    echo "Starting Wezterm Mux Server"
+    [ ! `pidof wezterm-mux-server` ] || wezterm-mux-server --daemonize > /dev/null
+    
+    echo "Starting Cron"
+    [ ! `pidof cron` ] || /etc/init.d/cron start > /dev/null
+
+}
+
 setup
+startup
 tail -f /dev/null
 
