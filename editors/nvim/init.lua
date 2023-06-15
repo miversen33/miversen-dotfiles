4238d411510c3ea3b54e7f2d1829ef9d0c5f8b55
Mike Iversen <miversen33@gmail.com>
2023-06-15 14:32:26 -0500

    blinky cursor go brrrr

--- editors/nvim/init.lua
+++ editors/nvim/init.lua
@@ -10,6 +10,7 @@ local function vim_settings()
     vim.opt.undolevels    = 1000
     vim.opt.undoreload    = 10000
     vim.opt.mouse         = 'a'
+    vim.opt.guicursor     = 'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175'
     vim.opt.whichwrap     = '<,>,[,]'
     vim.opt.encoding      = 'UTF-8'
     vim.log.level         = "warn"
