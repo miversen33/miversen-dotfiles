ac31d1c4a97955ec5595b5fda44aa4bafe9f2b03
Mike Iversen <miversen33@gmail.com>
2023-07-27 23:40:53 -0500

    Welcome back source selector!

--- editors/nvim/lua/plugins/init.lua
+++ editors/nvim/lua/plugins/init.lua
@@ -1025,6 +1025,15 @@ local function get_plugins()
             local neo_tree = require("neo-tree")
             local config = {
                 popup_border_style = 'rounded',
+                source_selector = {
+                        winbar = true,
+                        sources = {
+                            { source = "filesystem" },
+                            { source = "buffers" },
+                            { source = "document_symbols" },
+                            { source = "remote" },
+                        }
+                 },
                 default_component_configs = {
                     name = {
                         highlight_opened_files = "all",
@@ -1168,6 +1177,7 @@ local function get_plugins()
     },
     {
         "folke/edgy.nvim",
+        enabled = false,
         event = "VeryLazy",
         init = function()
             vim.opt.laststatus = 3
