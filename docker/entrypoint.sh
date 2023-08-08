eef18109d555046ea713471772d846e567b9d94a
Mike Iversen <miversen33@gmail.com>
2023-08-08 08:18:22 -0500

    Reworked dev container

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,140 +1,64 @@
 #!/usr/bin/bash
 
-DEBUG=1
-
-function do_log() {
-    if [ $DEBUG == 1 ]; then
-        echo "Entrypoint: $*"
-    fi
-}
-
-function start_ssh_server(){
-    /etc/init.d/ssh start
-}
-
 function setup(){
-    if [ -f /.setup_complete ]; then
-        do_log "Setup Complete"
-        return 0
+    if [ ! -d /home/miversen/.ssh ]; then
+        echo "Setting up ssh!"
+        mkdir -p /home/miversen/.ssh
+        chmod 664 /home/miversen/.ssh
+        touch /home/miversen/.ssh/authorized_keys
+        chmod 600 /home/miversen/.ssh/authorized_keys
+        touch /home/miversen/.ssh/known_hosts
+        chmod 640 /home/miversen/.ssh/known_hosts
+        [ -f /tmp/ssh_config ] && cp /tmp/ssh_config /home/miversen/.ssh/
+        [ -f /tmp/id_rsa.pub ] && cp /tmp/id_rsa.pub /home/miversen/.ssh/
+        [ -f /tmp/id_rsa ] && cp /tmp/id_rsa /home/miversen/.ssh/
+        [ -f /home/miversen/.ssh/id_rsa ] && chmod 600 /home/miversen/.ssh/id_rsa
+        [ -f /home/miversen/.ssh/id_rsa.pub ] && chmod 664 /home/miversen/.ssh/id_rsa.pub
+        chown miversen:miversen -R /home/miversen/.ssh
+        if [ -f /home/miversen/.ssh/config ]; then
+            control_path=`cat /home/miversen/.ssh/config | sed -n "s|^\s*ControlPath\s*['\"]\(.*\)%[a-zA-Z]['\"]$|\1|p"`
+            if [ $control_path ]; then
+                control_path="${control_path/#\~/home/miversen/}"
+                do_log "Ensuring $control_path exists"
+                mkdir -p $control_path
+            fi
+        fi
     fi
-    # do_log "Clearing out home directory"
-    # rm -rf ~/* ~/.* 2> /dev/null
-    if [ ! -f ~/.zshrc ]; then
-        do_log "Prepopulating ZSHRC"
-        touch ~/.zshrc
-        echo -n "echo \"ZSH is still setting up, please wait!\";exit 1;" > ~/.zshrc
-    fi
-
-
     DOTFILES_PATH="https://github.com/miversen33/miversen-dotfiles.git"
-    if [ ! -d ~/.ssh ] && [ -d /config ]; then
-        mkdir -p ~/.ssh
-        do_log "Found config directory, checking for ssh stuff"
-        [ -f /config/id_rsa ] && cp /config/id_rsa ~/.ssh/id_rsa && chown root:root ~/.ssh/id_rsa && chmod 400 ~/.ssh/id_rsa && DOTFILES_PATH="git@github.com:miversen33/miversen-dotfiles.git";
-        [ -f /config/id_rsa.pub ] && cp /config/id_rsa.pub ~/.ssh/id_rsa.pub && chown root:root ~/.ssh/id_rsa.pub && chmod 660 ~/.ssh/id_rsa.pub && cat ~/.ssh/id_rsa.pub > ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys;
-        [ -f /config/ssh_config ] && cp /config/ssh_config ~/.ssh/config && chown root:root ~/.ssh/config && chmod 600 ~/.ssh/config;
-    fi
-    if [ -f ~/.ssh/config ]; then
-        control_path=`cat ~/.ssh/config | sed -n "s|^\s*ControlPath\s*['\"]\(.*\)%[a-zA-Z]['\"]$|\1|p"`
-        if [ $control_path ]; then
-            control_path="${control_path/#\~/$HOME}"
-            do_log "Ensuring $control_path exists"
-            mkdir -p $control_path
-        fi
-    fi
-    if [ -f ~/.ssh/id_rsa ]; then
+    # Only do this if it isn't already in our known_hosts
+    if [ -f /home/miversen/.ssh/id_rsa ]; then
         do_log "Found SSH Key, pulling github pubkey"
+        DOTFILES_PATH="git@github.com:miversen33/miversen-dotfiles.git";
         GITHUB_KEY=`ssh-keyscan github.com 2> /dev/null`
-        echo $GITHUB_KEY >> ~/.ssh/known_hosts
+        echo $GITHUB_KEY >> /home/miversen/.ssh/known_hosts
     fi
-    if [ ! -d ~/.dotfiles ]; then
-        mkdir -p ~/.dotfiles ~/.config/{nvim,wezterm}
-        do_log "Reaching out to $DOTFILES_PATH"
-        git clone -q $DOTFILES_PATH ~/.dotfiles
-        cd ~/.dotfiles
-        do_log "Updating Submodules of $DOTFILES_PATH"
+    if [ ! -d /home/miversen/.dotfiles ]; then
+        echo "Downloading Dotfiles"
+        mkdir -p /home/miversen/.dotfiles /home/miversen/.config/{nvim,wezterm}
+        git clone -q $DOTFILES_PATH /home/miversen/.dotfiles
+        cd /home/miversen/.dotfiles
+#         do_log "Updating Submodules of $DOTFILES_PATH"
         git submodule update -q --init --recursive 2>&1 > /dev/null
-        do_log "Setting up links"
-        ln -s ~/.dotfiles/editors/nvim/* ~/.config/nvim/
-        ln -s ~/.dotfiles/terminals/wezterm/* ~/.config/wezterm/
-        ln -s ~/.dotfiles/terminals/user_wezconf.lua ~/.config/wezterm/
+#         do_log "Setting up links"
+        ln -s /home/miversen/.dotfiles/editors/nvim/* /home/miversen/.config/nvim/
+        ln -s /home/miversen/.dotfiles/terminals/wezterm/* /home/miversen/.config/wezterm/
+        ln -s /home/miversen/.dotfiles/terminals/user_wezconf.lua /home/miversen/.config/wezterm/
     fi
 
-    if [ ! -d ~/.oh-my-zsh ]; then
-        do_log "Setting up ohmyzsh"
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 2>&1 >/dev/null
-        git clone -q --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-        rm -rf ~/.zshrc ~/.p10k.zsh 2> /dev/null
-
-        if [ -d ~/.dotfiles/shells ]; then
-            ln -s ~/.dotfiles/shells/.shellrc ~/.zshrc
-            ln -s ~/.dotfiles/shells/.p10k.zsh ~/
-        fi
+    if [ -d /home/miversen/.dotfiles/shells ]; then
+        rm -rf /home/miversen/.zshrc /home/miversen/.p10k.zsh 2> /dev/null
+        ln -s /home/miversen/.dotfiles/shells/.shellrc /home/miversen/.zshrc
+        ln -s /home/miversen/.dotfiles/shells/.p10k.zsh /home/miversen/.p10k.zsh
     fi
-
-    if [ ! -f /usr/local/bin/lazygit ]; then
-        do_log "Setting up Lazygit"
-        cd /tmp/
-        LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-        curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-        tar xf lazygit.tar.gz lazygit
-        install lazygit /usr/local/bin
-    fi
-
-    if [ ! -f /usr/bin/tsserver ]; then
-        do_log "Installing Typescript"
-        npm install -g typescript 2>&1 > /dev/null
-    fi
-
-    if [ ! -d ~/workspace ]; then
-        do_log "Setting up workspace link to ~/workspace"
-        rm -rf ~/workspace
-        ln -s /workspace ~/workspace
-    fi
-    
-    do_log "Setting up ssh configuration"
-    if ! grep -qE 'AcceptEnv.*TERM' /etc/ssh/sshd_config; then
-        accept_env_var=$(grep -E 'AcceptEnv.*' /etc/ssh/sshd_config)
-        if [ ! -n "${accept_env_var}" ]; then
-            accept_env_var="AcceptEnv"
-        fi
-        accept_env_var="${accept_env_var} *TERM*";
-        sed -i 's/\(AcceptEnv.*\)/\1 TERM/' /etc/ssh/sshd_config
-    fi
-    if ! grep -Ei 'PubkeyAuthentication\s+yes' /etc/ssh/sshd_config; then
-        sed -i 's/^.*PubkeyAuthentication.*$/PubkeyAuthentication yes/' /etc/ssh/sshd_config
-    fi
-    sed -i 's/#PubkeyAuthentication/PubkeyAuthentication/' /etc/ssh/sshd_config
-
-    do_log "Setting default shell to /bin/.zsh"
-    chsh --shell /bin/zsh
-
+    rm /home/miversen/workspace
+    chown miversen:miversen -R /home/miversen
+    ln -s /workspace /home/miversen/
     touch /.setup_complete
-    echo "Dev Env Setup Complete"
 }
 
-function startup(){
-    echo "Starting Wezterm Mux Server"
-    if [ -x "$(command -v wezterm-mux-server)" ] && [ ! `pidof wezterm-mux-server` ]; then
-        do_log "Starting Wezterm Mux Server"
-        wezterm-mux-server --daemonize > /dev/null
-    fi
-    echo "Starting Cron"
-    [ `pidof cron` ] || /etc/init.d/cron start > /dev/null
-    echo "Starting SSH Server"
-    [ `pidof sshd` ] || /etc/init.d/ssh start > /dev/null
+function start(){
+    [ ! -f /.setup_complete ] && setup
+    su -l miversen
 }
 
-setup
-startup
-
-for i in "$@"
-do
-    case $i in
-        -t|--tail)
-            tail -f /dev/null;;
-        *)
-            zsh -l;;
-    esac
-done
+start
