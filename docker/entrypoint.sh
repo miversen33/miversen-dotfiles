fb4331dee9db4853a843328407999ddaa017f220
Mike Iversen <miversen33@gmail.com>
2023-08-08 20:56:21 +0000

    Fixing missing function call

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -19,7 +19,6 @@ function setup(){
             control_path=`cat /home/miversen/.ssh/config | sed -n "s|^\s*ControlPath\s*['\"]\(.*\)%[a-zA-Z]['\"]$|\1|p"`
             if [ $control_path ]; then
                 control_path="${control_path/#\~/home/miversen/}"
-                do_log "Ensuring $control_path exists"
                 mkdir -p $control_path
             fi
         fi
@@ -27,7 +26,6 @@ function setup(){
     DOTFILES_PATH="https://github.com/miversen33/miversen-dotfiles.git"
     # Only do this if it isn't already in our known_hosts
     if [ -f /home/miversen/.ssh/id_rsa ]; then
-        do_log "Found SSH Key, pulling github pubkey"
         DOTFILES_PATH="git@github.com:miversen33/miversen-dotfiles.git";
         GITHUB_KEY=`ssh-keyscan github.com 2> /dev/null`
         echo $GITHUB_KEY >> /home/miversen/.ssh/known_hosts
@@ -37,14 +35,11 @@ function setup(){
         mkdir -p /home/miversen/.dotfiles /home/miversen/.config/{nvim,wezterm}
         git clone -q $DOTFILES_PATH /home/miversen/.dotfiles
         cd /home/miversen/.dotfiles
-#         do_log "Updating Submodules of $DOTFILES_PATH"
         git submodule update -q --init --recursive 2>&1 > /dev/null
-#         do_log "Setting up links"
         ln -s /home/miversen/.dotfiles/editors/nvim/* /home/miversen/.config/nvim/
         ln -s /home/miversen/.dotfiles/terminals/wezterm/* /home/miversen/.config/wezterm/
         ln -s /home/miversen/.dotfiles/terminals/user_wezconf.lua /home/miversen/.config/wezterm/
     fi
-
     if [ -d /home/miversen/.dotfiles/shells ]; then
         rm -rf /home/miversen/.zshrc /home/miversen/.p10k.zsh 2> /dev/null
         ln -s /home/miversen/.dotfiles/shells/.shellrc /home/miversen/.zshrc
