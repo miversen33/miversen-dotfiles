f5ef0b91f938656b794bad3150f07a8b7a0c2903
Mike Iversen <miversen33@gmail.com>
2023-10-26 11:14:10 -0500

    Added auto recogonize to ccc

--- editors/nvim/lua/plugins/ide/ccc.lua
+++ editors/nvim/lua/plugins/ide/ccc.lua
@@ -2,6 +2,10 @@ local ccc_config = {
     highlighter = {
         auto_enable = true,
         excludes = _G.__miversen_config_excluded_filetypes_array
+    },
+    recognize = {
+        input = true,
+        output = true
     }
 }
 
