ec96da71bf1ae37370d434f1aa862c9564f2f992
Mike Iversen <miversen33@gmail.com>
2023-10-28 18:55:24 -0500

    Added proper exclusion of dropbar

--- editors/nvim/lua/plugins/ui/dropbar.lua
+++ editors/nvim/lua/plugins/ui/dropbar.lua
@@ -1,12 +1,35 @@
-return {
-    "Bekaboo/dropbar.nvim",
-    opts = {
-        icons = {
-            ui = {
-                bar = {
-                    separator = "  ",
-                }
+local function dropbar_ignore_buffer(buf, win)
+    local ft = vim.bo[buf].filetype
+    local acceptable =
+        not vim.api.nvim_win_get_config(win).zindex
+        and ft ~= ''
+        and vim.api.nvim_buf_get_name(buf) ~= ''
+        and not vim.wo[win].diff
+    for _, buftype in ipairs(_G.__miversen_config_excluded_filetypes_array) do
+        if ft == buftype then
+            acceptable = false
+            break
+        end
+    end
+    return acceptable
+end
+
+local dropbar_opts = {
+    general = {
+        enable = dropbar_ignore_buffer,
+    },
+    icons = {
+        ui = {
+            bar = {
+                separator = "  ",
             }
         }
     }
 }
+
+local dropbar = {
+    "Bekaboo/dropbar.nvim",
+    opts = dropbar_opts,
+}
+
+return dropbar
