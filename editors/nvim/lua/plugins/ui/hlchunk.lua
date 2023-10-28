d61a95f32a43760ba22e0c8adef1c6072dd59a4b
Mike Iversen <miversen33@gmail.com>
2023-10-28 18:55:45 -0500

    Fixed hlchunk not excluding properly

--- editors/nvim/lua/plugins/ui/hlchunk.lua
+++ editors/nvim/lua/plugins/ui/hlchunk.lua
@@ -1,23 +1,28 @@
+local exclude_filetypes = {}
+for _, ft in ipairs(_G.__miversen_config_excluded_filetypes_array) do
+    exclude_filetypes[ft] = true
+end
+
 local hlchunk_opts = {
     chunk = {
         enable = true,
         notify = false,
-        exclude_filetypes = _G.__miversen_config_excluded_filetypes_array
+        exclude_filetypes = exclude_filetypes
     },
     indent = {
         enable = true,
         notify = false,
-        exclude_filetypes = _G.__miversen_config_excluded_filetypes_array
+        exclude_filetypes = exclude_filetypes
     },
     line_num = {
         enable = true,
         notify = false,
-        exclude_filetypes = _G.__miversen_config_excluded_filetypes_array
+        exclude_filetypes = exclude_filetypes
     },
     blank = {
         enable = true,
         notify = false,
-        exclude_filetypes = _G.__miversen_config_excluded_filetypes_array
+        exclude_filetypes = exclude_filetypes
     }
 }
 
