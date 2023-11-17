244c42363096ea56bb05d3c52173c6c8545ec16a
Mike Iversen <miversen33@gmail.com>
2023-11-17 10:16:57 -0600

    Added exclusions for session persistence

--- editors/nvim/lua/plugins/ide/persisted.lua
+++ editors/nvim/lua/plugins/ide/persisted.lua
@@ -1,7 +1,13 @@
+
 local persisted_config = {
     use_git_branch = true,
     should_autosave = true,
     autoload = true,
+    ignored_dirs = {
+      vim.uv and vim.uv.os_homedir() or vim.loop.os_homedir(),
+      "/tmp",
+      "/",
+    }
 }
 
 local persisted = {
