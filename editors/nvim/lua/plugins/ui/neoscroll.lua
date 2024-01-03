da3db912e0426af65c16542c6de50dabd1e56a6a
Mike Iversen <miversen33@gmail.com>
2024-01-03 13:27:37 -0600

    Added S-k and S-j as PageUp and PageDown

--- editors/nvim/lua/plugins/ui/neoscroll.lua
+++ editors/nvim/lua/plugins/ui/neoscroll.lua
@@ -1,14 +1,13 @@
 local neoscroll_opts = {}
 local neoscroll_mappings = {
     ['<PageUp>']   = {'scroll', { '-vim.api.nvim_win_get_height(0) + 1 ', 'true', '200'}},
+    ['<S-k>']      = {'scroll', { '-vim.api.nvim_win_get_height(0) + 1 ', 'true', '200'}},
     ['<PageDown>'] = {'scroll', { 'vim.api.nvim_win_get_height(0) - 1', 'true', '200'}},
+    ['<S-j>']      = {'scroll', { 'vim.api.nvim_win_get_height(0) - 1', 'true', '200'}},
     ['<Home>'] = {'scroll', { '-vim.api.nvim_win_get_cursor(0)[1]', 'true', '50'}},
     ['<S-Home>'] = {'scroll', {'vim.api.nvim_buf_line_count(0) - vim.api.nvim_win_get_cursor(0)[1]', 'true', '50'}},
     ['<S-Up>'] = {'scroll', { '-(vim.api.nvim_win_get_height(0) / 2)', 'false', '200'}},
     ['<S-Down>'] = {'scroll', {'vim.api.nvim_win_get_height(0) / 2', 'false', '200'}}
-
-    -- ['<PageUp>']   = {'scroll', { '-vim.wo.scroll', 'true', '250'}},
-    -- ['<PageDown>'] = {'scroll', { 'vim.wo.scroll', 'true', '250'}}
 }
 
 
