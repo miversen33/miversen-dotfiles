6ed89429129c7943f87131c0a7527a8e9e8b9b0c
Mike Iversen <miversen33@gmail.com>
2024-01-02 21:07:17 -0600

    Updated fidget

--- editors/nvim/lua/plugins/ui/fidget.lua
+++ editors/nvim/lua/plugins/ui/fidget.lua
@@ -1,8 +1,21 @@
-local fidget_opts = {}
+local fidget_opts = {
+    progress = {
+        display = {
+            render_limit = 10,
+        },
+        ignore_done_already = true,
+        ignore_empty_message = true,
+    },
+    notification = {
+        window = {
+            winblend = 0,
+            border = "rounded",
+        }
+    }
+}
 
 local fidget = {
     "j-hui/fidget.nvim",
-    tag = "legacy",
     event = "LspAttach",
     opts = fidget_opts
 }
