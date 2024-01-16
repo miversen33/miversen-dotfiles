c5bb2b9476d400d2441474f32358f61bed763c79
Mike Iversen <miversen33@gmail.com>
2024-01-15 21:01:47 -0600

    Added persisted to telescope

--- editors/nvim/lua/plugins/ui/telescope.lua
+++ editors/nvim/lua/plugins/ui/telescope.lua
@@ -2,11 +2,19 @@ local telescope_dependencies = {
     'nvim-lua/plenary.nvim'
 }
 
+local telescope_extensions = {
+    "persisted"
+}
+
 local telescope = {
     "nvim-telescope/telescope.nvim",
     dependencies = telescope_dependencies,
-    cmd = {"Telescope"}
-
+    cmd = {"Telescope"},
+    config = function()
+        for _, extension in ipairs(telescope_extensions) do
+            require("telescope").load_extension(extension)
+        end
+    end
 }
 
 return telescope
