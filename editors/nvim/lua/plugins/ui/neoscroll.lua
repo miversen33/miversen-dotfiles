839f28fd6154618648820e5d583a96b33348d3aa
Mike Iversen <miversen33@gmail.com>
2024-01-15 22:10:57 -0600

    Updated neoscrll to handle holding down keys better

--- editors/nvim/lua/plugins/ui/neoscroll.lua
+++ editors/nvim/lua/plugins/ui/neoscroll.lua
@@ -1,24 +1,76 @@
-local neoscroll_opts = {}
-local neoscroll_mappings = {
-    ['<PageUp>']   = {'scroll', { '-vim.api.nvim_win_get_height(0) + 1 ', 'true', '200'}},
-    ['<S-k>']      = {'scroll', { '-vim.api.nvim_win_get_height(0) + 1 ', 'true', '200'}},
-    ['<PageDown>'] = {'scroll', { 'vim.api.nvim_win_get_height(0) - 1', 'true', '200'}},
-    ['<S-j>']      = {'scroll', { 'vim.api.nvim_win_get_height(0) - 1', 'true', '200'}},
-    ['<Home>'] = {'scroll', { '-vim.api.nvim_win_get_cursor(0)[1]', 'true', '50'}},
-    ['<S-Home>'] = {'scroll', {'vim.api.nvim_buf_line_count(0) - vim.api.nvim_win_get_cursor(0)[1]', 'true', '50'}},
-    ['<S-Up>'] = {'scroll', { '-(vim.api.nvim_win_get_height(0) / 2)', 'false', '200'}},
-    ['<S-Down>'] = {'scroll', {'vim.api.nvim_win_get_height(0) / 2', 'false', '200'}}
+local is_scrolling = false
+
+local neoscroll_opts = {
+    respect_scrolloff = true,
+    pre_hook = function() is_scrolling = true end,
+    post_hook = function() is_scrolling = false end,
 }
+
+-- TODO: Probably could add some "nice" centering of the cursor on scroll
+
+local neoscroll_config = function()
+    local neoscroll = require("neoscroll")
+    neoscroll.setup(neoscroll_opts)
+    local modes = {"n", "v", "i", "t"}
+    local reg_time_step = 200
+    local fast_time_step = 50
+    vim.keymap.set(modes, '<PageUp>', function()
+        if is_scrolling then return end
+        neoscroll.scroll(-vim.api.nvim_win_get_height(0) + 1, true, reg_time_step)
+    end)
+
+    vim.keymap.set(modes, "<PageDown>", function()
+        if is_scrolling then return end
+        neoscroll.scroll(vim.api.nvim_win_get_height(0) - 1, true, reg_time_step)
+    end)
+
+    vim.keymap.set(modes, "<S-k>", function()
+        if is_scrolling then return end
+        neoscroll.scroll(-vim.api.nvim_win_get_height(0) - 1, true, reg_time_step)
+    end)
+
+    vim.keymap.set(modes, "<S-j>", function()
+        if is_scrolling then return end
+        neoscroll.scroll(vim.api.nvim_win_get_height(0) + 1, true, reg_time_step)
+    end)
+
+    vim.keymap.set(modes, "<S-Up>", function()
+        if is_scrolling then return end
+        neoscroll.scroll(-(vim.api.nvim_win_get_height(0) / 2), false, reg_time_step)
+    end)
+
+    vim.keymap.set(modes, "<S-Down>", function()
+        if is_scrolling then return end
+        neoscroll.scroll(-(vim.api.nvim_win_get_height(0) / 2), false, reg_time_step)
+    end)
+
+    vim.keymap.set(modes, "<Home>", function()
+        if is_scrolling then return end
+        neoscroll.scroll(-vim.api.nvim_win_get_cursor(0)[1], true, fast_time_step)
+    end)
+
+    vim.keymap.set(modes, "<End>", function()
+        if is_scrolling then return end
+        neoscroll.scroll(vim.api.nvim_win_get_cursor(0)[1], true, fast_time_step)
+    end)
+
+    vim.keymap.set(modes, "<S-Home>", function()
+        if is_scrolling then return end
+        neoscroll.scroll(vim.api.nvim_buf_line_count(0) - vim.api.nvim_win_get_cursor(0)[1], true, fast_time_step)
+    end)
+
+    vim.keymap.set(modes, "<S-End>", function()
+        if is_scrolling then return end
+        neoscroll.scroll(vim.api.nvim_buf_line_count(0) + vim.api.nvim_win_get_cursor(0)[1], true, fast_time_step)
+    end)
+
+    require("neoscroll.config").set_mappings()
+end
 
 
 local neoscroll = {
     "karb94/neoscroll.nvim",
-    config = function()
-        local neoscroll = require("neoscroll")
-        neoscroll.setup(neoscroll_opts)
-        require("neoscroll.config").set_mappings(neoscroll_mappings)
-        -- vim.keymap.set()
-    end
+    config = neoscroll_config
 }
 
 return neoscroll
