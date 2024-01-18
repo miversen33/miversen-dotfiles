9a529fc5a8bd79eb6b7b162954d41be4abfa8bc2
Mike Iversen <miversen33@gmail.com>
2024-01-18 16:28:40 -0600

    Switched to true ripgrep

--- editors/nvim/lua/plugins/ui/telescope.lua
+++ editors/nvim/lua/plugins/ui/telescope.lua
@@ -1,15 +1,20 @@
 local telescope_dependencies = {
-    'nvim-lua/plenary.nvim'
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope-live-grep-args.nvim'
 }
 
 local telescope_extensions = {
     "persisted"
 }
 
+local telescope_options = {
+}
+
 local telescope = {
     "nvim-telescope/telescope.nvim",
     dependencies = telescope_dependencies,
     cmd = {"Telescope"},
+    opts = telescope_options,
     config = function()
         for _, extension in ipairs(telescope_extensions) do
             require("telescope").load_extension(extension)
