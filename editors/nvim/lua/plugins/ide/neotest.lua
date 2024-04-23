38908b7d1acf49b8c21d9d0c33bd098603a2986f
Mike Iversen <miversen33@gmail.com>
2024-04-23 08:24:32 -0500

    Added nio and more test frameworks

--- editors/nvim/lua/plugins/ide/neotest.lua
+++ editors/nvim/lua/plugins/ide/neotest.lua
@@ -21,7 +21,12 @@ local neotest_opts = {
        -- , {
        --     args = { "-E 'not test(/raw/)'" }
        -- }),
+       lazy_req("neotest-go"),
+       lazy_req("neotest-vitest"),
+       lazy_req("neotest-bash"),
+       lazy_req("neotest-haskell"),
        lazy_req("neotest-zig"),
+       lazy_req("neotest-playwrite"),
        lazy_req("neotest-python"),
        lazy_req("neotest-java")
     }
@@ -36,6 +41,12 @@ local neotest = {
         "nvim-neotest/neotest-python",
         "rcasia/neotest-java",
         "lawrence-laz/neotest-zig",
+        "nvim-neotest/nvim-nio",
+        "nvim-neotest/neotest-go",
+        "marilari88/neotest-vitest",
+        "rcasia/neotest-bash",
+        "mrcjkb/neotest-haskell",
+        "thenbe/neotest-playwright",
     },
     config = true,
     opts = neotest_opts
