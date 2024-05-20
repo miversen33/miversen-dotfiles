ac5a6896296cb48cfa20c9003577377ec8bce572
Mike Iversen <miversen33@gmail.com>
2024-05-20 07:58:06 -0500

    Adding nvim-java-refactor

--- editors/nvim/lua/plugins/ide/mason.lua
+++ editors/nvim/lua/plugins/ide/mason.lua
@@ -164,6 +164,7 @@ local mason_dependencies = {
         "nvim-java/nvim-java",
         dependencies = {
             'nvim-java/lua-async-await',
+            'nvim-java/nvim-java-refactor',
             'nvim-java/nvim-java-core',
             'nvim-java/nvim-java-test',
             'nvim-java/nvim-java-dap',
