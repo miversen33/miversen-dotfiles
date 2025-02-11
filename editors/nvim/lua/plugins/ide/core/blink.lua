28f8aabb627b76854513661ec3aabce27400bb84
Mike Iversen <miversen33@gmail.com>
2025-02-11 15:09:48 -0600

    Added blink.cmp

--- editors/nvim/lua/plugins/ide/core/blink.lua
+++ editors/nvim/lua/plugins/ide/core/blink.lua
@@ -2,64 +2,147 @@ vim.api.nvim_set_hl(0, 'BlinkCmpMenuBorder', { fg = _G.__miversen_border_color }
 vim.api.nvim_set_hl(0, "BlinkCmpLabelDeprecated", { fg = "#7E8294", bg = "NONE", strikethrough = true })
 vim.api.nvim_set_hl(0, "BlinkCmpLabelMatch", { fg = "#82AAFF", bg = "NONE", bold = true })
 vim.api.nvim_set_hl(0, "BlinkCmpMenu", { fg = "#C792EA", bg = "NONE", italic = true })
+vim.api.nvim_set_hl(0, "BlinkCmpGhostText", { fg = "#7E8294", bg = "NONE" })
 
-vim.api.nvim_set_hl(0, "BlinkCmpKindField", { fg = "#EED8DA", bg = "#B5585F" })
-vim.api.nvim_set_hl(0, "BlinkCmpKindProperty", { fg = "#EED8DA", bg = "#B5585F" })
-vim.api.nvim_set_hl(0, "BlinkCmpKindEvent", { fg = "#EED8DA", bg = "#B5585F" })
-
-vim.api.nvim_set_hl(0, "BlinkCmpKindText", { fg = "#C3E88D", bg = "#9FBD73" })
-vim.api.nvim_set_hl(0, "BlinkCmpKindEnum", { fg = "#C3E88D", bg = "#9FBD73" })
-vim.api.nvim_set_hl(0, "BlinkCmpKindKeyword", { fg = "#C3E88D", bg = "#9FBD73" })
-
-vim.api.nvim_set_hl(0, "BlinkCmpKindConstant", { fg = "#FFE082", bg = "#D4BB6C" })
-vim.api.nvim_set_hl(0, "BlinkCmpKindConstructor", { fg = "#FFE082", bg = "#D4BB6C" })
-vim.api.nvim_set_hl(0, "BlinkCmpKindReference", { fg = "#FFE082", bg = "#D4BB6C" })
-
-vim.api.nvim_set_hl(0, "BlinkCmpKindFunction", { fg = "#EADFF0", bg = "#A377BF" })
-vim.api.nvim_set_hl(0, "BlinkCmpKindStruct", { fg = "#EADFF0", bg = "#A377BF" })
-vim.api.nvim_set_hl(0, "BlinkCmpKindClass", { fg = "#EADFF0", bg = "#A377BF" })
-vim.api.nvim_set_hl(0, "BlinkCmpKindModule", { fg = "#EADFF0", bg = "#A377BF" })
-vim.api.nvim_set_hl(0, "BlinkCmpKindOperator", { fg = "#EADFF0", bg = "#A377BF" })
-
-vim.api.nvim_set_hl(0, "BlinkCmpKindVariable", { fg = "#C5CDD9", bg = "#7E8294" })
-vim.api.nvim_set_hl(0, "BlinkCmpKindFile", { fg = "#C5CDD9", bg = "#7E8294" })
-
-vim.api.nvim_set_hl(0, "BlinkCmpKindUnit", { fg = "#F5EBD9", bg = "#D4A959" })
-vim.api.nvim_set_hl(0, "BlinkCmpKindSnippet", { fg = "#F5EBD9", bg = "#D4A959" })
-vim.api.nvim_set_hl(0, "BlinkCmpKindFolder", { fg = "#F5EBD9", bg = "#D4A959" })
-
-vim.api.nvim_set_hl(0, "BlinkCmpKindMethod", { fg = "#DDE5F5", bg = "#6C8ED4" })
-vim.api.nvim_set_hl(0, "BlinkCmpKindValue", { fg = "#DDE5F5", bg = "#6C8ED4" })
-vim.api.nvim_set_hl(0, "BlinkCmpKindEnumMember", { fg = "#DDE5F5", bg = "#6C8ED4" })
-
-vim.api.nvim_set_hl(0, "BlinkCmpKindInterface", { fg = "#D8EEEB", bg = "#58B5A8" })
-vim.api.nvim_set_hl(0, "BlinkCmpKindColor", { fg = "#D8EEEB", bg = "#58B5A8" })
-vim.api.nvim_set_hl(0, "BlinkCmpKindTypeParameter", { fg = "#D8EEEB", bg = "#58B5A8" })
+-- vim.api.nvim_set_hl(0, "BlinkCmpKindField", { fg = "#B5585F" })
+-- vim.api.nvim_set_hl(0, "BlinkCmpKindProperty", { fg = "#B5585F" })
+-- vim.api.nvim_set_hl(0, "BlinkCmpKindEvent", { fg = "#B5585F" })
+--
+-- vim.api.nvim_set_hl(0, "BlinkCmpKindText", { fg = "#9FBD73" })
+-- vim.api.nvim_set_hl(0, "BlinkCmpKindEnum", { fg = "#9FBD73" })
+-- vim.api.nvim_set_hl(0, "BlinkCmpKindKeyword", { fg = "#9FBD73" })
+--
+-- vim.api.nvim_set_hl(0, "BlinkCmpKindConstant", { fg = "#D4BB6C" })
+-- vim.api.nvim_set_hl(0, "BlinkCmpKindConstructor", { fg = "#D4BB6C" })
+-- vim.api.nvim_set_hl(0, "BlinkCmpKindReference", { fg = "#D4BB6C" })
+--
+-- vim.api.nvim_set_hl(0, "BlinkCmpKindFunction", { fg = "#A377BF" })
+-- vim.api.nvim_set_hl(0, "BlinkCmpKindStruct", { fg = "#A377BF" })
+-- vim.api.nvim_set_hl(0, "BlinkCmpKindClass", { fg = "#A377BF" })
+-- vim.api.nvim_set_hl(0, "BlinkCmpKindModule", { fg = "#A377BF" })
+-- vim.api.nvim_set_hl(0, "BlinkCmpKindOperator", { fg = "#A377BF" })
+--
+-- vim.api.nvim_set_hl(0, "BlinkCmpKindVariable", { fg = "#7E8294" })
+-- vim.api.nvim_set_hl(0, "BlinkCmpKindFile", { fg = "#7E8294" })
+--
+-- vim.api.nvim_set_hl(0, "BlinkCmpKindUnit", { fg = "#D4A959" })
+-- vim.api.nvim_set_hl(0, "BlinkCmpKindSnippet", { fg = "#D4A959" })
+-- vim.api.nvim_set_hl(0, "BlinkCmpKindFolder", { fg = "#D4A959" })
+--
+-- vim.api.nvim_set_hl(0, "BlinkCmpKindMethod", { fg = "#6C8ED4" })
+-- vim.api.nvim_set_hl(0, "BlinkCmpKindValue", { fg = "#6C8ED4" })
+-- vim.api.nvim_set_hl(0, "BlinkCmpKindEnumMember", { fg = "#6C8ED4" })
+--
+-- vim.api.nvim_set_hl(0, "BlinkCmpKindInterface", { fg = "#58B5A8" })
+-- vim.api.nvim_set_hl(0, "BlinkCmpKindColor", { fg = "#58B5A8" })
+-- vim.api.nvim_set_hl(0, "BlinkCmpKindTypeParameter", { fg = "#58B5A8" })
 
 
 ---@module "lazy"
 ---@type LazySpec
 return {
     'saghen/blink.cmp',
-    dependencies = 'rafamadriz/friendly-snippets',
-    version = '0.11.0',
+    dependencies = {'rafamadriz/friendly-snippets'},
+    version = '*',
 
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
-        keymap = { preset = 'default' },
+        keymap = {
+            preset = 'default',
+            ["<C-Space>"] = { 
+                ---@module 'blink.cmp'
+                ---@param cmp blink.cmp.API
+                function(cmp)
+                    if cmp.is_menu_visible() then
+                        cmp.select_and_accept()
+                    else
+                        cmp.show()
+                    end
+                -- "select_and_accept", "fallback",
 
+                end
+            },
+                
+            ["<C-Up>"] = {
+                ---@module 'blink.cmp'
+                ---@param cmp blink.cmp.API
+                function(cmp)
+                    cmp.scroll_documentation_up(4)
+                end
+            },
+            ["<C-Down>"] = {
+                ---@module 'blink.cmp'
+                ---@param cmp blink.cmp.API
+                function(cmp)
+                    cmp.scroll_documentation_down(4)
+                end
+            }
+        },
         appearance = {
             use_nvim_cmp_as_default = true,
             nerd_font_variant = 'mono',
         },
         completion = {
-            menu = { border = 'rounded' },
-            documentation = { window = { border = 'rounded' } }
+            ghost_text = {
+                enabled = true
+            },
+            documentation = {
+                auto_show = true,
+                auto_show_delay_ms = 500,
+                window = {
+                    border = 'rounded'
+                },
+            },
+            menu = {
+                border = 'rounded',
+                draw = {
+                    columns = {
+                        { 'kind_icon', gap = 1 },
+                        { "label", "label_description", gap = 1 },
+                        { 'kind' },
+                        { 'source_name' },
+                        { 'source_id' }
+                    },
+                    -- components = {
+                    --     label = {
+                    --         ---@module 'blink.cmp.completion.windows.render.context'
+                    --         ---@param ctx blink.cmp.DrawItemContext
+                    --         highlight = function(ctx)
+                    --             highlight = vim.api.nvim_get_hl(0, { name = ctx.item }) or {}
+                    --             if not highlight.fg then
+                    --                 return {}
+                    --             end
+                    --             return {
+                    --                 ctx.item,
+                    --                 range = 5
+                    --             }
+                    --         end
+                    --     }
+                    -- },
+                    -- components = {
+                    --     kind_icon = function(ctx)
+                    --         print(ctx)
+                    --         return ctx
+                    --     end
+                    -- },
+                    treesitter = { 'lsp' }
+                },
+            },
         },
-        signature = { window = { border = 'rounded' } },
+        signature = {
+            window = { border = 'rounded' },
+            -- enabled = true,
+        },
         sources = {
-            default = {'lsp', 'path', 'snippets', 'buffer' }
+            default = {'lsp', 'path', 'snippets', 'buffer' },
+            providers = {
+                lazydev = {
+                    name = "LazyDev",
+                    module = "lazydev.integrations.blink",
+                    score_offset = 100
+                }
+            }
         }
     },
     opts_extended = { 'sources.default' }
