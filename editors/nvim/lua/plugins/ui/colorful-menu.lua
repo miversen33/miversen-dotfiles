459781d2eb17add0078739fb04bd5610cb7c22cd
Mike Iversen <miversen33@gmail.com>
2025-07-27 12:07:39 -0500

    More colors for blink

---
+++ editors/nvim/lua/plugins/ui/colorful-menu.lua
@@ -0,0 +1,9 @@
+---@module "lazy"
+---@type LazySpec
+return {
+    "xzbdmw/colorful-menu.nvim",
+    config = function()
+        -- You don't need to set these options.
+        require("colorful-menu").setup({})
+    end
+}
