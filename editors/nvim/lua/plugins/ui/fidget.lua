d543c06713be8672a03f829b3b83f7e4701e9f16
Mike Iversen <miversen33@gmail.com>
2025-07-27 12:07:51 -0500

    Added fidget

---
+++ editors/nvim/lua/plugins/ui/fidget.lua
@@ -0,0 +1,25 @@
+---@module "lazy"
+local fidget_opts = {
+    progress = {
+        display = {
+            render_limit = 10,
+        },
+        ignore_done_already = true,
+        ignore_empty_message = true,
+    },
+    notification = {
+        window = {
+            winblend = 0,
+            border = "rounded",
+        }
+    }
+}
+
+---@type LazySpec
+local fidget = {
+    "j-hui/fidget.nvim",
+    event = "LspAttach",
+    opts = fidget_opts
+}
+
+return fidget
