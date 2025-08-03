ccdb3298979c2da1f3d6ef404e6c3c859bff0a1c
Mike Iversen <miversen33@gmail.com>
2025-08-03 08:51:51 -0500

    Snacks

--- editors/nvim/lua/plugins/ui/snacks.lua
+++ editors/nvim/lua/plugins/ui/snacks.lua
@@ -1,14 +1,12 @@
----@module "lazy"
 ---@type LazySpec
 local snacks = {
-  "folke/snacks.nvim",
-  priority = 1000,
-  lazy = false,
-  ---@module "snacks"
-  ---@type snacks.Config
-  opts = {
-    input = { enabled = true },
-  },
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+        input = { enabled = true },
+    },
 }
 
 return snacks
