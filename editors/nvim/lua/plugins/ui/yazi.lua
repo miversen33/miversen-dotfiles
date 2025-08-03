314e0b17a1d56641cc56bd6e0987b844d1b8d50e
Mike Iversen <miversen33@gmail.com>
2025-08-03 08:52:55 -0500

    Setup yazi

--- editors/nvim/lua/plugins/ui/yazi.lua
+++ editors/nvim/lua/plugins/ui/yazi.lua
@@ -1,38 +1,36 @@
----@module "lazy"
----@module "yazi"
 ---@type LazySpec
 local yazi = {
-  "mikavilpas/yazi.nvim",
-  event = "VeryLazy",
-  dependencies = {
-    -- check the installation instructions at
-    -- https://github.com/folke/snacks.nvim
-    "folke/snacks.nvim",
-    "echasnovski/mini.nvim"
-  },
-  keys = {
-    -- 👇 in this section, choose your own keymappings!
-    {
-      "fy",
-      mode = { "n", "v" },
-      "<cmd>Yazi toggle<cr>",
-      desc = "Open yazi at the current file",
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        -- check the installation instructions at
+        -- https://github.com/folke/snacks.nvim
+        "folke/snacks.nvim",
+        "echasnovski/mini.nvim"
     },
-  },
-  ---@type YaziConfig | {}
-  opts = {
-    -- if you want to open yazi instead of netrw, see below for more info
-    open_for_directories = false,
-    keymaps = {
-      show_help = "<f1>",
+    keys = {
+        -- 👇 in this section, choose your own keymappings!
+        {
+            "fy",
+            mode = { "n", "v" },
+            "<cmd>Yazi toggle<cr>",
+            desc = "Open yazi at the current file",
+        },
     },
-  },
-  -- 👇 if you use `open_for_directories=true`, this is recommended
-  init = function()
-    -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
-    -- vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
-  end,
+    ---@type YaziConfig | {}
+    opts = {
+        -- if you want to open yazi instead of netrw, see below for more info
+        open_for_directories = false,
+        keymaps = {
+            show_help = "<f1>",
+        },
+    },
+    -- 👇 if you use `open_for_directories=true`, this is recommended
+    init = function()
+        -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+        -- vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+    end,
 }
 
 return yazi
