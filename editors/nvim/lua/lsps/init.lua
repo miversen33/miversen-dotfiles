e5da95edc2d023ca62ea39d06a1f199ce725ab18
Mike Iversen <miversen33@gmail.com>
2025-08-08 15:51:02 -0500

    Added better support for external lsp interaction

--- editors/nvim/lua/lsps/init.lua
+++ editors/nvim/lua/lsps/init.lua
@@ -4,7 +4,7 @@ local lsp_manager = require("scripts.lsp")
 local M = {}
 
 ---@type Lsp[]
-M.lsps = {}
+M._lsps = {}
 
 local lsp_dir = string.format("%s/lua/lsps", vim.fn.stdpath("config"))
 local lsp_files = vim.fn.glob(lsp_dir .. "/*.lua", false, true)
@@ -26,29 +26,33 @@ for _, file_path in ipairs(lsp_files) do
         vim.notify(string.format("LSP module %s did not return a table", filename), vim.log.levels.WARN)
         goto continue
     end
-    -- If the module returns a single LSP config, wrap it in an array
-    if lsp_module.name then
-        table.insert(M.lsps, lsp_module)
-    else
-        vim.list_extend(M.lsps, lsp_module)
+    -- Wrap the lsp_module in a table so we can iterate
+    local _lsps = lsp_module.name and { lsp_module } or lsp_module
+    for _, _lsp in ipairs(_lsps) do
+        table.insert(M._lsps, _lsp)
+        -- Group lsps by their filetypes so we can more easily manipulate them
+        if _lsp.config.filetypes then
+            for _, filetype in ipairs(_lsp.config.filetypes) do
+                if M[filetype] == nil then
+                    M[filetype] = {}
+                end
+                M[filetype][_lsp.name] = _lsp
+            end
+        end
     end
     ::continue::
 end
 
--- Optional: Set up autocmds for all discovered LSPs
-function M.setup()
-    for _, lsp in ipairs(M.lsps) do
-        if lsp.enable ~= false then -- Default to enabled unless explicitly disabled
-            vim.api.nvim_create_autocmd("FileType", {
-                pattern = lsp.config.filetypes or {},
-                callback = function(ev)
-                    lsp_manager.register(lsp.config.filetypes, lsp)
-                end,
-                desc = string.format("Auto lsp handler for %s", lsp.name),
-                once = true -- There is never a reason to have this fire more than once ever
-            })
-        end
-    end
+for _, lsp in pairs(M._lsps) do
+    vim.api.nvim_create_autocmd("FileType", {
+        pattern = lsp.config.filetypes or {},
+        callback = function(ev)
+            lsp_manager.register(lsp.config.filetypes, lsp)
+        end,
+        desc = string.format("Auto lsp handler for %s", lsp.name),
+        once = true -- There is never a reason to have this fire more than once ever
+    })
 end
+::continue::
 
 return M
