eeb7e189fdb401796d783ff2c923f12a45dc1711
Mike Iversen <michael.iversen@interpublic.com>
2025-08-25 09:37:13 -0500

    Fixed issues around invalid size and permission handling

--- shells/yazi/init.lua
+++ shells/yazi/init.lua
@@ -1,19 +1,24 @@
+-- init.lua
 function Linemode:custom_linemode()
     local time = math.floor(self._file.cha.mtime or 0)
     if time == 0 then
         time = ""
     else
         time = os.date("%H:%M:%S %Y-%m-%d", time)
+
     end
-
     local size = self._file:size()
-
-    local perms = self._file.cha:perm()
-    local username = ya.user_name(self._file.cha.uid) or ""
-    local group = ya.group_name(self._file.cha.gid) or ""
+    local perms = self._file.cha:perm() or ""
+    local username = self._file.cha.uid and ya.user_name(self._file.cha.uid) or ""
+    local group = self._file.cha.gid and ya.group_name(self._file.cha.gid) or ""
     local ownership = ""
-    -- if username and group then
-    --     ownership = string.format(" %s:%s", username, group)
-    -- end
-    return string.format("%s %s %s%s", size and ya.readable_size(size) or "-", time, perms, ownership)
+    if username:len() > 0 and group:len() > 0 then
+        ownership = string.format(" %s:%s", username, group)
+    end
+    if size then
+        size = ya.readable_size(size)
+    else
+        size = "-"
+    end
+    return string.format("%s %s %s%s", size, time, perms, ownership)
 end
