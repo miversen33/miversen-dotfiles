2f154f2f2b06bb78570ae6647d024c4fe8bb41c6
Mike Iversen <miversen33@gmail.com>
2025-09-18 21:50:04 -0500

    formatting gud

--- editors/nvim/init.lua
+++ editors/nvim/init.lua
@@ -70,15 +70,15 @@ local function vim_settings()
         }
     end
     vim.g.clipboard = {
-      name = 'OSC 52',
-      copy = {
-        ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-        ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
-      },
-      paste = {
-        ['+'] = paste,
-        ['*'] = paste,
-      },
+        name = 'OSC 52',
+        copy = {
+            ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+            ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+        },
+        paste = {
+            ['+'] = paste,
+            ['*'] = paste,
+        },
     }
 
     vim.opt.background = 'dark'
