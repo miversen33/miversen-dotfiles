a7856fce30321210dba8a655037bf18e6fa063ce
Mike Iversen <miversen33@gmail.com>
2025-10-04 08:20:19 -0500

    Why on earth did I think this was a good idea?

--- editors/nvim/init.lua
+++ editors/nvim/init.lua
@@ -313,16 +313,17 @@ local function setup_plugins()
 
     vim.g._config = editor_config
 
-    local lazypath = "/dev/shm/lazy.nvim"
-    -- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
     local lazyurl = "https://github.com/folke/lazy.nvim.git"
     local lazybranch = "stable"
+
     if not vim.uv.fs_stat(lazypath) then
         vim.fn.system({
             "git", "clone", "--filter=blob:none", lazyurl,
             string.format("--branch=%s", lazybranch), lazypath
         })
     end
+
     vim.opt.rtp:prepend(lazypath)
     local success, lazy_or_err = pcall(require, "lazy")
     if not success then
